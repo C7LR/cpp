7eacd4a723cc67891981a0ae25942b6237aef1bd
C7LR <cesarjulianlozada@usf.edu>
2024-10-06 18:37:09 +0000

    commit

---
+++ AHCP#11.c
@@ -0,0 +1,34 @@
+/*Cesar Lozada*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+void gradePrinter(int *gradeList, int listLength){
+    for(int i=0; i<listLength; ++i){
+        printf("%d ", gradeList[i]);
+    }
+    printf("\n\n");
+}
+
+
+void switcher(int *gradeList, int listLength){
+    for(int i=0; i<listLength; ++i){
+        if((gradeList[i]<70)&&(gradeList[i]>59)){
+            gradeList[i] += 10;
+        }
+    }
+}
+
+int main(){
+    int finalExam[15]={90,82,65,79,67,82,94,64,88,78,92,61,96,83,74};
+
+    printf("Scores before changing:\n");
+    gradePrinter(finalExam,15);
+    
+    switcher(finalExam,15);
+
+    printf("Scores after changing:\n");
+    gradePrinter(finalExam,15);
+
+    return 0;
+}
